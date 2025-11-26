8909a20689d16e8e7290a6e3a16ee00ed3f00b02
Mats Kindahl <mats@kindahl.net>
2025-11-26 08:19:32 +0100

    chore: adding copyright to all files

--- Makefile
+++ Makefile
@@ -1,3 +1,19 @@
+# InfluxDB API to PostgreSQL. Copyright (C) 2025 Mats Kindahl
+#    
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#    
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
+
 MODULE_big = influxdb
 OBJS = influxdb.o parser.o tokenizer_lex.o utils.o plans.o insert.o table.o
 
--- influxdb.h
+++ influxdb.h
@@ -1,9 +1,27 @@
-#ifndef INFLUXDB_H_
-#define INFLUXDB_H_
+/*
+ * InfluxDB API to PostgreSQL. Copyright (C) 2025 Mats Kindahl
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef INFLUXDB_INFLUXDB_H_
+#define INFLUXDB_INFLUXDB_H_
 
 #include <stdbool.h>
 
 extern bool influxdb_keep_quotes;
 extern bool influxdb_auto_create_table;
 
-#endif /* INFLUXDB_H_ */
+#endif /* INFLUXDB_INFLUXDB_H_ */
