c28be3db3a14533613de0d2d3507cf9c883a86d8
Mats Kindahl <mats@kindahl.net>
2026-01-29 14:21:11 +0100

    chore: move shared TAP code into module
    There are some functions that are shared by all TAP tests so these are
    moved into a dedicated module and placed in the `perl/` directory.

--- t/001_http_write.pl
+++ t/001_http_write.pl
@@ -1,85 +1,27 @@
-use strict;
+# InfluxDB API to PostgreSQL. Copyright (C) 2025 Mats Kindahl
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see
+# <https://www.gnu.org/licenses/>.
 
-#use warnings FATAL => 'all';
+use strict;
+use warnings FATAL => 'all';
+
+use lib 'perl';
 
 use PostgreSQL::Test::Cluster;
 use Test::More;
-use IPC::Run;
-use HTTP::Response;
-use List::Util qw(all);
-use JSON       qw(decode_json);
-use Data::Dumper;
-
-use constant {
-    NO_CONTENT  => 204,
-    BAD_REQUEST => 400,
-    NOT_FOUND   => 404,
-};
-
-my %reason = (
-    204 => 'No Content',
-    400 => 'Bad Request',
-    404 => 'Not Found',
-);
-
-sub trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s; }
-
-sub curl {
-    my ( $url, $input ) = @_;
-    my ( $stdin, $stdout, $stderr );
-
-    my @cmd = (
-        "curl",          "-is", "--header", "Content-Type: text/plain",
-        "--header",      "Connection: close",
-        "--data-binary", $input, $url
-    );
-
-    IPC::Run::run \@cmd, \$stdin, \$stdout, \$stderr;
-    return $stdout;
-}
-
-sub is_response {
-    my ( $response, $code, $message ) = @_;
-    my $tb = Test::More->builder;
-
-    $tb->is_eq( $response->message, $message );
-    $tb->is_eq( $response->code,    $code );
-
-}
-
-sub has_headers {
-    my ( $response, @headers ) = @_;
-    my $tb = Test::More->builder;
-    $tb->ok( all { $response->header($_) } @headers );
-}
-
-sub test_endpoint {
-    my ( $endpoint, $input, $code, $check ) = @_;
-    my $reason   = $reason{$code};
-    my $output   = curl $endpoint, $input;
-    my $response = HTTP::Response->parse($output);
-    my $tb       = Test::More->builder;
-    my @headers  = ( 'Date', 'Connection' );
-    my $json     = decode_json( $response->content ) if $response->content;
-    my $errmsg   = "generated error '" . $json->{'error'} . "'"
-      if $response->content;
-
-    $tb->ok( all { $response->header($_) } @headers );
-    $tb->is_eq( $response->message, $reason, $errmsg );
-    $tb->is_eq( $response->code,    $code,   $errmsg );
-    $check->($response) if defined $check;
-}
-
-sub has_error {
-    my ($error) = @_;
-    my $func = sub {
-        my ($response) = @_;
-        my $tb         = Test::More->builder;
-        my $json       = decode_json( $response->content );
-        return $tb->like( $json->{'error'}, $error );
-    };
-    return $func;
-}
+use InfluxDB::Extras ':all';
 
 my ( $output, $response, $json, $expected, $result );
 
