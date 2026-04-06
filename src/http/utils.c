55c83ec3b72270d49ea216a82dbbd586cc8624f2
Mats Kindahl <mats@kindahl.net>
2026-04-06 13:56:10 +0200

    fix(http): add HTTP method validation for all endpoints
    Validate the HTTP method in `on_url()` against a per-endpoint bitmask.
    Return `405 Method Not Allowed` with an `Allow` header for unsupported
    methods.
    
    Allowed methods: `/write` support `POST`, `/ping` supports `GET` and
    `HEAD`, and `/query` supports `GET` and `POST`.

--- src/http/utils.c
+++ src/http/utils.c
@@ -1,9 +1,29 @@
+/*
+ * Copyright (C) 2025 Mats Kindahl
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 #include "http/utils.h"
 
 #include <postgres.h>
 #include <fmgr.h>
 
 #include <utils/builtins.h>
+
+#include "http/http_parser.h"
 
 PG_FUNCTION_INFO_V1(url_decode_text);
 
@@ -45,4 +65,21 @@ void url_decode(char* s) {
     }
   }
   *dst = '\0';
+}
+
+/*
+ * Function: allow_header_string
+ * Description: Generates the Allow header string for the given allowed methods.
+ */
+const char* allow_header_string(unsigned int allowed_methods) {
+  StringInfoData buf;
+  initStringInfo(&buf);
+  for (int i = 0; i < 8 * sizeof(allowed_methods); ++i) {
+    if (allowed_methods & (1 << i)) {
+      if (buf.len > 0)
+        appendStringInfoString(&buf, ", ");
+      appendStringInfoString(&buf, http_method_str(i));
+    }
+  }
+  return buf.data;
 }
