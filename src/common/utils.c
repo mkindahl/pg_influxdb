de85893c5490a8ed17d49489c838f001958f5b44
Mats Kindahl <mats@kindahl.net>
2026-04-06 16:49:22 +0200

    chore: add copyright check workflow and normalize copyright notices
    Add a CI workflow that checks all hand-written source files have a
    correctly formatted copyright notice. Normalize existing notices. Add
    missing notices to files that lacked them.

--- src/common/utils.c
+++ src/common/utils.c
@@ -1,5 +1,5 @@
 /*
- * InfluxDB API to PostgreSQL. Copyright (C) 2025 Mats Kindahl
+ * Copyright (C) 2025 Mats Kindahl
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License
